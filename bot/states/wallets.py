6dedd2a6b5fe6467a83d7e9063e91c7b7ac8e446
Creator <SolSenseiDev@gmail.com>
2025-06-09 20:21:06 +0300

    Add files via upload

--- bot/states/wallets.py
+++ bot/states/wallets.py
@@ -2,3 +2,7 @@ from aiogram.fsm.state import StatesGroup, State
 
 class WalletStates(StatesGroup):
     waiting_for_private_key = State()
+    waiting_for_withdraw_address = State()
+    waiting_for_withdraw_amount = State()
+    waiting_for_withdraw_usdc_address = State()
+    waiting_for_withdraw_usdc_amount = State()
