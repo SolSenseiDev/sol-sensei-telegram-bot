07e1f40f657acc0e64900201291ed0988b188692
Creator <SolSenseiDev@gmail.com>
2025-06-25 22:43:04 +0300

    Delete bot/states directory

--- bot/states/buy_sell.py
+++
@@ -1,9 +0,0 @@
-from aiogram.fsm.state import StatesGroup, State
-
-
-class BuySellStates(StatesGroup):
-    waiting_for_ca = State()
-    choosing_mode = State()  # Buy or Sell
-    selecting_wallets = State()  # Selecting wallets to use
-    entering_buy_amount = State()  # For custom buy input
-    entering_sell_percent = State()  # For custom sell input
--- bot/states/swap_states.py
+++
@@ -1,5 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-class SwapState(StatesGroup):
-    fixed_sol_to_usdc_amount = State()
-    fixed_usdc_to_sol_amount = State()
